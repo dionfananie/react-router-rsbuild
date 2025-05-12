e4233720b23a206a6246e0358b83c1954c7d5b37
Dion Fananie <dionfananie@gmail.com>
2025-05-12 09:05:44 +0700

    add cloudflare

--- app/welcome/welcome.tsx
+++ app/welcome/welcome.tsx
@@ -1,7 +1,7 @@
-import logoDark from "./logo-dark.svg";
-import logoLight from "./logo-light.svg";
+import logoDark from './logo-dark.svg';
+import logoLight from './logo-light.svg';
 
-export function Welcome() {
+export function Welcome({ message }: { message: string }) {
   return (
     <main className="flex items-center justify-center pt-16 pb-4">
       <div className="flex-1 flex flex-col items-center gap-16 min-h-0">
@@ -38,6 +38,7 @@ export function Welcome() {
                   </a>
                 </li>
               ))}
+              <li className="self-stretch p-3 leading-normal">{message}</li>
             </ul>
           </nav>
         </div>
@@ -48,8 +49,8 @@ export function Welcome() {
 
 const resources = [
   {
-    href: "https://reactrouter.com/docs",
-    text: "React Router Docs",
+    href: 'https://reactrouter.com/docs',
+    text: 'React Router Docs',
     icon: (
       <svg
         xmlns="http://www.w3.org/2000/svg"
@@ -68,8 +69,8 @@ const resources = [
     ),
   },
   {
-    href: "https://rmx.as/discord",
-    text: "Join Discord",
+    href: 'https://rmx.as/discord',
+    text: 'Join Discord',
     icon: (
       <svg
         xmlns="http://www.w3.org/2000/svg"
--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -1,18 +1,41 @@
-import { defineConfig } from "@rsbuild/core";
-import { pluginReactRouter } from "rsbuild-plugin-react-router";
-import { pluginReact } from "@rsbuild/plugin-react";
+import { defineConfig } from '@rsbuild/core';
+import { pluginReact } from '@rsbuild/plugin-react';
+import { pluginReactRouter } from 'rsbuild-plugin-react-router';
 
 export default defineConfig({
-  plugins: [
-    pluginReactRouter({
-      // Optional: Enable custom server mode
-      customServer: false,
-      // Optional: Specify server output format
-      serverOutput: "commonjs",
-      //Optional: enable experimental support for module federation
-      federation: false,
-    }),
-    ,
-    pluginReact(),
-  ],
+  environments: {
+    node: {
+      performance: {
+        // cloudflare cannot support dynamic chunk split in worker
+        chunkSplit: { strategy: 'all-in-one' },
+      },
+      tools: {
+        rspack: {
+          // must use esm module output
+          experiments: {
+            outputModule: true,
+          },
+          externalsType: 'module',
+          output: {
+            chunkFormat: 'module',
+            chunkLoading: 'import',
+            workerChunkLoading: 'import',
+            wasmLoading: 'fetch',
+            library: { type: 'module' },
+            module: true,
+          },
+          resolve: {
+            conditionNames: [
+              'workerd',
+              'worker',
+              'browser',
+              'import',
+              'require',
+            ],
+          },
+        },
+      },
+    },
+  },
+  plugins: [pluginReactRouter({customServer: true}), pluginReact()],
 });
